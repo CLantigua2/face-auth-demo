5db3692ac28c202254509f3a0e5a63281e57bfe7
Carlos Lantigua <clantigua954@gmail.com>
2021-06-25 19:29:49 -0400

    setup deps and added webcam and canvas to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const camRef = useRef();
+	const canvasRef = useRef();
+
+	return (
+		<div className="App">
+			<div className="App-header">
+				<Webcam
+					ref={camRef}
+					style={{
+						position: "absolute",
+						margin: "0 auto",
+						left: 0,
+						right: 0,
+						width: 720,
+						height: 480,
+					}}
+				/>
+				<canvas
+					ref={canvasRef}
+					style={{
+						position: "absolute",
+						margin: "0 auto",
+						left: 0,
+						right: 0,
+						width: 720,
+						height: 480,
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
+/**
+ * steps
+ * 1. intall dependencies
+ *  a) TensorflowJS
+ *  b) facial landmark detection model
+ *  c) React webcam
+ * 2. Setup Webcam and canvas
+ * 3. Load facemesh
+ * 4. Detect  func
+ * 5. Draw utilities
+ * 6. Load triangulation
+ * 7. Setup triangle path
+ * 8. Setup point drawing
+ * 9. Add drawMesh to detect function
+ */
