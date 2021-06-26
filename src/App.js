1f04e4136de93026dca37c888dd10259718f21fe
Carlos Lantigua <clantigua954@gmail.com>
2021-06-25 21:44:33 -0400

    added canvas keypoints over the camera

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect, useCallback } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
@@ -7,6 +7,56 @@ import "./App.css";
 function App() {
 	const camRef = useRef();
 	const canvasRef = useRef();
+	let timer = useRef();
+	let mesh;
+
+	useEffect(() => {
+		loadFacemesh();
+	}, []);
+
+	useEffect(() => {
+		timer.current = setInterval(() => {
+			detect(mesh);
+		}, 5000);
+		return () => clearInterval(timer.current);
+	}, [mesh]);
+
+	// Load our facemesh
+	const loadFacemesh = useCallback(async () => {
+		mesh = await facemesh.load();
+	}, [mesh]);
+
+	// Detect Function
+	const detect = async (mesh) => {
+		// checks if our camera video is ready
+		// should return 4 on ready
+		if (camRef?.current?.video?.readyState === 4) {
+			const video = camRef.current.video;
+			const videoWidth = video.videoWidth;
+			const videoHeight = video.videoHeight;
+			camRef.current.video.width = videoWidth;
+			camRef.current.video.height = videoHeight;
+			canvasRef.current.width = videoWidth;
+			canvasRef.current.height = videoHeight;
+
+			const predictions = await mesh.estimateFaces(camRef.current.video);
+			if (predictions.length > 0) {
+				for (let i = 0; i < predictions.length; i++) {
+					const keypoints = predictions[i].scaledMesh;
+
+					// Log facial keypoints.
+					for (let i = 0; i < keypoints.length; i++) {
+						const [x, y, z] = keypoints[i];
+						const ctx = canvasRef.current.getContext("2d");
+						ctx.beginPath();
+						ctx.arc(x, y, 1, 0, 3 * Math.PI);
+						ctx.fillStyle = "aqua";
+						ctx.fill();
+					}
+				}
+			}
+		}
+	};
 
 	return (
 		<div className="App">
